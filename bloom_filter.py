c0c76bc439c25120c6aa242778c4b28c2ca1a2e1
mansoor <mansurkhurram@yahoo.com>
2026-06-04 05:04:48 +0200

    added hash position generation

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,3 +1,4 @@
+import hashlib
 import math
 
 class BloomFilter:
@@ -5,13 +6,31 @@ class BloomFilter:
         self.expected_items = expected_items
         self.false_positive_rate = false_positive_rate
 
+        # size of the bit array
         self.size = math.ceil(
             -(expected_items * math.log(false_positive_rate)) / (math.log(2) ** 2)
         )
 
+        # number of hash functions
         self.number_of_hashes = math.ceil(
             (self.size / expected_items) * math.log(2)
         )
 
+        # start with all bits set to 0
         self.bit_array = [0] * self.size
+
         self.items_added = 0
+
+    def get_hash_positions(self, item):
+        positions = []
+
+        # same item, but slightly changed each time using i
+        # this gives us different hash positions
+        for i in range(self.number_of_hashes):
+            text = str(item) + str(i)
+            hash_result = hashlib.sha256(text.encode()).hexdigest()
+            position = int(hash_result, 16) % self.size
+
+            positions.append(position)
+
+        return positions
